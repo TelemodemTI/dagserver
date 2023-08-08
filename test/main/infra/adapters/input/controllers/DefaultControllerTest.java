9e8ed09d1c69f9d8f704e5b16f0a0d86d6f04242
Diego Lira <maximolira@users.noreply.github.com>
2023-08-08 12:05:29 -0400

    fix(test)
    change pom configuration to add Jacoco and Surefire plugins

---
+++ test/main/infra/adapters/input/controllers/DefaultControllerTest.java
@@ -0,0 +1,31 @@
+package main.infra.adapters.input.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DefaultControllerTest {
+
+	private DefaultController controller = new DefaultController(); 
+	
+    @Test
+    public void versionTest() {
+    	Model model = mock(Model.class);
+    	HttpServletRequest request = mock(HttpServletRequest.class);
+    	HttpServletResponse response = mock(HttpServletResponse.class);
+    	try {
+    		if(controller.version(model,request,response).getStatusCode().is2xxSuccessful()) {
+        		assertTrue(true); 	
+        	} else {
+        		assertTrue(false);
+        	}	
+		} catch (Exception e) {
+			assertTrue(false,e.getMessage());
+		}
+    }
+}
