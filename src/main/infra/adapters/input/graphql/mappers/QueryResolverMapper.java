fd703f6a691cfbbce7020012f47ef3692450d3c8
Diego Lira <maximolira@users.noreply.github.com>
2023-08-23 19:49:35 -0400

    fix(mapStruct)

--- src/main/infra/adapters/input/graphql/mappers/QueryResolverMapper.java
+++ src/main/infra/adapters/input/graphql/mappers/QueryResolverMapper.java
@@ -1,7 +1,6 @@
 package main.infra.adapters.input.graphql.mappers;
 
-import org.springframework.stereotype.Component;
-
+import org.mapstruct.Mapper;
 import main.domain.model.AgentDTO;
 import main.domain.model.PropertyDTO;
 import main.domain.model.UncompiledDTO;
@@ -11,43 +10,11 @@ import main.infra.adapters.input.graphql.types.Agent;
 import main.infra.adapters.input.graphql.types.Property;
 import main.infra.adapters.input.graphql.types.Uncompiled;
 
-@Component
-public class QueryResolverMapper {
-
-	public Agent toAgent(AgentDTO dto) {
-		Agent agent = new Agent();
-		agent.setHostname(dto.getHostname());
-		agent.setId(dto.getId());
-		agent.setName(dto.getName());
-		agent.setUpdatedOn(dto.getUpdatedOn());
-		return agent;
-	}
+@Mapper(componentModel = "spring")
+public interface QueryResolverMapper {
 	
-	public Property toProperty(PropertyDTO dto) {
-		Property prop = new Property();
-		prop.setDescription(dto.getDescription());
-		prop.setGroup(dto.getGroup());
-		prop.setName(dto.getName());
-		prop.setValue(dto.getValue());
-		return prop;
-	}
-	
-	public Uncompiled toUncompiled(UncompiledDTO dto) {
-		Uncompiled un = new Uncompiled();
-		un.setBin(dto.getBin());
-		un.setCreatedDt(dto.getCreatedDt());
-		un.setUncompiledId(dto.getUncompiledId());
-		return un;
-	}
-
-	public Account toAccount(UserDTO elt) {
-		Account acc = new Account();
-		acc.setId(elt.getId());
-		acc.setTypeAccount(elt.getTypeAccount());
-		acc.setUsername(elt.getUsername());
-		return acc;
-	}
-
-	
-	
+	public Agent toAgent(AgentDTO dto);	
+	public Property toProperty(PropertyDTO dto);
+	public Uncompiled toUncompiled(UncompiledDTO dto);
+	public Account toAccount(UserDTO elt);
 }
--- src/main/infra/adapters/output/repositories/mappers/SchedulerMapper.java
+++ src/main/infra/adapters/output/repositories/mappers/SchedulerMapper.java
@@ -1,6 +1,6 @@
 package main.infra.adapters.output.repositories.mappers;
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
 import main.domain.model.EventListenerDTO;
 import main.domain.model.LogDTO;
@@ -11,45 +11,12 @@ import main.infra.adapters.output.repositories.entities.Log;
 import main.infra.adapters.output.repositories.entities.PropertyParameter;
 import main.infra.adapters.output.repositories.entities.User;
 
-@Component
-public class SchedulerMapper {
-	public LogDTO toLogDTO(Log log){
-		LogDTO dto = new LogDTO();
-		dto.setDagname(log.getDagname());
-		dto.setExecDt(log.getExecDt());
-		dto.setId(log.getId());
-		dto.setOutputxcom(log.getOutputxcom());
-		dto.setStatus(log.getStatus());
-		dto.setValue(log.getValue());
-		return dto;
-	}
+@Mapper(componentModel = "spring")
+public interface SchedulerMapper {
+
+	public LogDTO toLogDTO(Log log);
+	public EventListenerDTO toEventListenerDTO(EventListener eventListener);
+	public UserDTO toUserDTO(User user);
+	public PropertyParameterDTO toPropertyParameterDTO(PropertyParameter prop);
 	
-	public EventListenerDTO toEventListenerDTO(EventListener eventListener) {
-		EventListenerDTO dto = new EventListenerDTO();
-		dto.setGroupName(eventListener.getGroupName());
-		dto.setListenerName(eventListener.getListenerName());
-		dto.setOnEnd(eventListener.getOnEnd());
-		dto.setOnStart(eventListener.getOnStart());
-		return dto;
-	}
-	
-	public UserDTO toUserDTO(User user) {
-		UserDTO dto = new UserDTO();
-		dto.setCreatedAt(user.getCreatedAt());
-		dto.setId(user.getId());
-		dto.setPwdhash(user.getPwdhash());
-		dto.setTypeAccount(user.getTypeAccount());
-		dto.setUsername(user.getUsername());
-		return dto;
-	}
-	
-	public PropertyParameterDTO toPropertyParameterDTO(PropertyParameter prop) {
-		PropertyParameterDTO dto = new PropertyParameterDTO();
-		dto.setDescription(prop.getDescription());
-		dto.setGroup(prop.getGroup());
-		dto.setId(prop.getId());
-		dto.setName(prop.getName());
-		dto.setValue(prop.getValue());
-		return dto;
-	}
 }
