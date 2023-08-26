b8bca03fcd07008f5a212232991083fb5a64d166
Diego Lira <maximolira@users.noreply.github.com>
2023-08-26 13:53:38 -0400

    Update DefaultController.java

--- src/main/infra/adapters/input/controllers/DefaultController.java
+++ src/main/infra/adapters/input/controllers/DefaultController.java
@@ -1,5 +1,8 @@
 package main.infra.adapters.input.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
@@ -8,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -26,5 +30,12 @@ public class DefaultController {
 		RedirectView redirectView = new RedirectView();
         redirectView.setUrl("/dagserver/cli/index.html");
         return redirectView;
+	}
+	@PostMapping(path = "/github-trigger")
+	public ResponseEntity<String> githubEvent(Model model,HttpServletRequest request,HttpServletResponse response){
+		logger.debug(request);
+		var ndate = new Date();
+		var sdf = new SimpleDateFormat("yyyy-MM-ddThh:mm:ss");
+		return new ResponseEntity<>("event raised at "+sdf.format(ndate), HttpStatus.OK);
 	}
 }
