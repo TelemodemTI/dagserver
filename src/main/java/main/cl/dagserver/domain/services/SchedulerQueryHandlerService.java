6e529775d0dc5cfcf984b5dc1ad534808a3f7d2a
Diego Lira <maximolira@users.noreply.github.com>
2024-06-02 15:55:46 -0400

    Update SchedulerQueryHandlerService.java

--- src/main/java/main/cl/dagserver/domain/services/SchedulerQueryHandlerService.java
+++ src/main/java/main/cl/dagserver/domain/services/SchedulerQueryHandlerService.java
@@ -184,7 +184,7 @@ public class SchedulerQueryHandlerService extends BaseServiceComponent implement
 	    List<ChannelDTO> channels = new ArrayList<>();
 	    
 	    channels.add(createChannel("SCHEDULER", "ACTIVE", "scheduler.png", Collections.emptyList()));
-	    channels.add(createChannel("GITHUB_CHANNEL", getChannelStatus("GITHUB"), "github.png", getChannelProps("GITHUB")));
+	    channels.add(createChannel("GITHUB_CHANNEL", getChannelStatus("GITHUB_WEBHOOK_PROPS"), "github.png", getChannelProps("GITHUB_WEBHOOK_PROPS")));
 	    channels.add(createChannel(RABBITMQ, getChannelStatus(RABBITMQ), "rabbit.png", getChannelProps(RABBITMQ)));
 	    channels.add(createChannel(REDIS_LISTENER, getChannelStatus(REDIS_LISTENER), "redis.png", getChannelProps(REDIS_LISTENER)));
 	    channels.add(createChannel(KAFKA_CONSUMER, getChannelStatus(KAFKA_CONSUMER), "kafka.png", getChannelProps(KAFKA_CONSUMER)));
