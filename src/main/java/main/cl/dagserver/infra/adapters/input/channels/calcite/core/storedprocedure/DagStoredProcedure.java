fdb6b3b91e6cab4e7016d213b4aa4e0865f0df01
Diego Lira <maximolira@users.noreply.github.com>
2024-09-27 17:54:06 -0300

    fix
    sonarqube

--- src/main/java/main/cl/dagserver/infra/adapters/input/channels/calcite/core/storedprocedure/DagStoredProcedure.java
+++ src/main/java/main/cl/dagserver/infra/adapters/input/channels/calcite/core/storedprocedure/DagStoredProcedure.java
@@ -2,23 +2,27 @@ package main.cl.dagserver.infra.adapters.input.channels.calcite.core.storedproce
 
 import org.json.JSONObject;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEventPublisher;
+
 import main.cl.dagserver.application.ports.input.StageApiUsecase;
+import main.cl.dagserver.domain.core.ExceptionEventLog;
 import main.cl.dagserver.domain.exceptions.DomainException;
 import main.cl.dagserver.infra.adapters.confs.ApplicationContextUtils;
 
-public class DagStoredProcedure {
+public class DagStoredProcedure  {
 
     @SuppressWarnings("static-access")
 	public static int run(String jarname,String dagname,String args) {
     	ApplicationContext appCtx = new ApplicationContextUtils().getApplicationContext();
     	StageApiUsecase api = appCtx.getBean("stageApiService", StageApiUsecase.class);
+    	ApplicationEventPublisher eventPublisher = appCtx.getBean("eventPublisher", ApplicationEventPublisher.class);
     	try {
     		args = args.trim().isEmpty()?"{}":args.trim();
     		var obj = new JSONObject(args);
 			api.executeDag(jarname, dagname, obj);
 			return 0;
 		} catch (DomainException e) {
-			e.printStackTrace();
+			eventPublisher.publishEvent(new ExceptionEventLog(DagStoredProcedure.class, new DomainException(e), "JDBC CALCITE DagStoredProcedure"));;
 			return -1;
 		}
     	 
