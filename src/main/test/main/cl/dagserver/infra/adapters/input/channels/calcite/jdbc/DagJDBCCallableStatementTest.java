8ce2df5b2751b40acb96ac236b28dbd361c84126
Diego Lira <maximolira@users.noreply.github.com>
2024-10-17 19:32:02 -0300

    feat
    add unit test

---
+++ src/main/test/main/cl/dagserver/infra/adapters/input/channels/calcite/jdbc/DagJDBCCallableStatementTest.java
@@ -0,0 +1,101 @@
+package main.cl.dagserver.infra.adapters.input.channels.calcite.jdbc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.sql.SQLException;
+
+public class DagJDBCCallableStatementTest {
+
+    private DagJDBCCallableStatement callableStatement;
+    private DagJDBCConnection mockConnection;
+    private String testSql = "SELECT * FROM users";
+    
+    @BeforeEach
+    public void setup() {
+        mockConnection = Mockito.mock(DagJDBCConnection.class);
+    }
+
+    @Test
+    public void testExecuteQuery() throws SQLException {
+        DagJDBCResultSet mockResultSet = Mockito.mock(DagJDBCResultSet.class);
+        callableStatement = Mockito.spy(new DagJDBCCallableStatement(mockConnection, testSql));
+        
+        Mockito.doReturn(mockResultSet).when(callableStatement).executeQuery(testSql);
+        
+        DagJDBCResultSet result = callableStatement.executeQuery();
+        assertNotNull(result);
+        assertEquals(mockResultSet, result);
+        
+        Mockito.verify(callableStatement).executeQuery(testSql);
+    }
+    @Test
+    public void testExecuteQuerySql() throws SQLException {
+        DagJDBCResultSet mockResultSet = Mockito.mock(DagJDBCResultSet.class);
+        callableStatement = Mockito.spy(new DagJDBCCallableStatement(mockConnection, testSql));
+        
+        Mockito.doReturn(mockResultSet).when(callableStatement).executeQuery(testSql);
+        
+        DagJDBCResultSet result = callableStatement.executeQuery("select 1");
+        assertNotNull(result);
+        assertEquals(mockResultSet, result);
+        
+        Mockito.verify(callableStatement).executeQuery(testSql);
+    }
+    
+    @Test
+    public void testExecute() throws SQLException {
+        DagJDBCResultSet mockResultSet = Mockito.mock(DagJDBCResultSet.class);
+        callableStatement = Mockito.spy(new DagJDBCCallableStatement(mockConnection, testSql));
+        
+        Mockito.doReturn(mockResultSet).when(callableStatement).executeQuery(testSql);
+        
+        boolean result = callableStatement.execute();
+        assertNotNull(result);
+        assertEquals(mockResultSet, result);
+        
+        Mockito.verify(callableStatement).executeQuery(testSql);
+    }
+    
+    @Test
+    public void testExecuteQuery2() throws SQLException {
+        DagJDBCResultSet mockResultSet = Mockito.mock(DagJDBCResultSet.class);
+        callableStatement = Mockito.spy(new DagJDBCCallableStatement(mockConnection, testSql,1,1));
+        
+        Mockito.doReturn(mockResultSet).when(callableStatement).executeQuery(testSql);
+        
+        DagJDBCResultSet result = callableStatement.executeQuery();
+        assertNotNull(result);
+        assertEquals(mockResultSet, result);
+        
+        Mockito.verify(callableStatement).executeQuery(testSql);
+    }
+    
+    @Test
+    public void testExecuteQuery3() throws SQLException {
+        DagJDBCResultSet mockResultSet = Mockito.mock(DagJDBCResultSet.class);
+        callableStatement = Mockito.spy(new DagJDBCCallableStatement(mockConnection, testSql,1,1,1));
+        
+        Mockito.doReturn(mockResultSet).when(callableStatement).executeQuery(testSql);
+        
+        DagJDBCResultSet result = callableStatement.executeQuery();
+        assertNotNull(result);
+        assertEquals(mockResultSet, result);
+        
+        Mockito.verify(callableStatement).executeQuery(testSql);
+    }
+
+    @Test
+    public void testExecuteUpdate() throws SQLException {
+        callableStatement = Mockito.spy(new DagJDBCCallableStatement(mockConnection, testSql));
+        
+        Mockito.doReturn(1).when(callableStatement).executeUpdate(testSql);
+        
+        int rowsUpdated = callableStatement.executeUpdate();
+        assertEquals(1, rowsUpdated);
+        
+        Mockito.verify(callableStatement).executeUpdate(testSql);
+    }
+}
