d062dab1e2d7c7a31d575b59627e6c2bb3e2d752
Diego Lira <maximolira@users.noreply.github.com>
2025-06-27 19:19:30 -0400

    test
    DockerFile

--- src/main/test/main/cl/dagserver/integration/test/OperatorFileTest.java
+++ src/main/test/main/cl/dagserver/integration/test/OperatorFileTest.java
@@ -1,14 +1,52 @@
 package main.cl.dagserver.integration.test;
 
+import org.junit.jupiter.api.Assertions;
 import org.testng.annotations.Test;
 
+import main.cl.dagserver.integration.pom.AuthenticatedPage;
+import main.cl.dagserver.integration.pom.JobsPage;
+import main.cl.dagserver.integration.pom.LoginPage;
+import main.cl.dagserver.integration.pom.segments.CanvasDagEditor;
+import main.cl.dagserver.integration.pom.segments.EditorParamModal;
 import main.cl.dagserver.integration.test.core.BaseOperatorTest;
 
 public class OperatorFileTest extends BaseOperatorTest {
 
     @Test(priority = 1)
-    public void readFileWTitleWoRDelimiterOperator() {
-        //lectura de archivo existe, con titulos, sin delimitador
+    public void readFileWTitleWoRDelimiterOperator() throws InterruptedException {
+        String dagname = "TEST_FILE1_DAG";
+        String step = "step1";
+        String group = "group.test";
+        String jarname = "filetest1.jar";
+        LoginPage loginPage = new LoginPage(this.driver);
+        if(loginPage.login("dagserver", "dagserver")){
+        	AuthenticatedPage authenticatedPage = new AuthenticatedPage(this.driver);
+        	JobsPage jobsPage = authenticatedPage.goToJobs();
+        	CanvasDagEditor canvas = createJob(jobsPage,jarname, dagname, group, step);
+            canvas.addStep(dagname,step,"main.cl.dagserver.infra.adapters.operators.FileOperator");
+            EditorParamModal params = canvas.selectStage(step);
+            params.selectTab("//*[@id=\"home_li\"]/a");
+            params.sendParameter("mode", "read", "list");
+            params.sendParameter("firstRowTitles", "true", "list");
+            params.sendParameter("rowDelimiter", ";", "input");
+            params.selectTab("//*[@id=\"file_li\"]/a");
+            params.selectFile("/prueba.csv");
+            params.save();
+            canvas.saveJar();
+        	jobsPage = authenticatedPage.goToJobs();
+        	var status = executeDesign(step, jarname, dagname,jobsPage);
+        	if(!status.isEmpty()) {
+        		if(status.getJSONObject(0).get("prueba").equals("valor1")) {
+        			authenticatedPage.goToJobs();
+                    authenticatedPage.logout();
+        			Assertions.assertTrue(true);
+        		} else {
+        			Assertions.fail("Problema al ejecutar el operador?");
+        		}
+        	} else {
+        		Assertions.fail("Problema al ejecutar el operador?");
+        	}
+        }
     }
     @Test(priority = 2)
     public void readFileWoTitleWoRDelimiterOperator() {
