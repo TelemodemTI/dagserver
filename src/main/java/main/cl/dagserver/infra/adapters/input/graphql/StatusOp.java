66c31a2094bcaede9c9e10fafad031abf543530b
Diego Lira <maximolira@users.noreply.github.com>
2025-07-27 10:50:23 -0400

    fix
    apply lombok in some classes

--- src/main/java/main/cl/dagserver/infra/adapters/input/graphql/StatusOp.java
+++ src/main/java/main/cl/dagserver/infra/adapters/input/graphql/StatusOp.java
@@ -1,26 +1,10 @@
 package main.cl.dagserver.infra.adapters.input.graphql;
 
+import lombok.Data;
+
+@Data
 public class StatusOp {
 	private String status;
     private Integer code;
     private String value;
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public Integer getCode() {
-		return code;
-	}
-	public void setCode(Integer code) {
-		this.code = code;
-	}
-	public String getValue() {
-		return value;
-	}
-	public void setValue(String value) {
-		this.value = value;
-	}
-    
 }
